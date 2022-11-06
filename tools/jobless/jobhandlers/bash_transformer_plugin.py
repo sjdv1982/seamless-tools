248fa5b107c40ca82bc79c96e2adf8c19375d87b
Sjoerd de Vries <sjdv1982@gmail.com>
2022-11-06 20:38:24 +0100

    minor

--- tools/jobless/jobhandlers/bash_transformer_plugin.py
+++ tools/jobless/jobhandlers/bash_transformer_plugin.py
@@ -11,7 +11,7 @@ class BashTransformerPlugin(FileTransformerPluginBase):
         '54a3024fde08ac6526e31651730068482d61fb9d2cebb7a0bd18d455e7bbbeb6', # Seamless 0.6-devel
         '3eb91d8f14b22be1823cc54b4c888f00e1ad214bbda25bdc8de64b47051a499a', # Seamless 0.6
         '1cee47f07959d9a4899599a4ad230d3cd11058ab130666001d10d8fe8fbd842f', # Seamless 0.7
-        '59e918e039a9b8a584ce9ba90b78941a8024ff9ff98db1133d67112974b0659f', # Seamless 0.8
+        '59e918e039a9b8a584ce9ba90b78941a8024ff9ff98db1133d67112974b0659f', # Seamless 0.8/0.9
     ]
     def required_pin_handler(self, pin, transformation):
         assert pin in self.REQUIRED_TRANSFORMER_PINS
