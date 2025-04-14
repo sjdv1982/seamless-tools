b3cf737bf62d23332bb8e5c575795d4d57b45308
Sjoerd de Vries <sjdv1982@gmail.com>
2025-04-14 16:25:53 +0200

    add --deep option to buffer size

--- scripts/buffer-size.py
+++ scripts/buffer-size.py
@@ -2,6 +2,7 @@
 
 # TODO: document
 
+import json
 import os
 import sys
 
@@ -30,9 +31,31 @@ parser.add_argument(
     action="store_true",
 )
 
+parser.add_argument(
+    "-d",
+    "--deep",
+    "--dict",
+    dest="is_dict",
+    help="Checksum file is a deep cell (dict of checksums)",
+    action="store_true",
+)
+
+parser.add_argument(
+    "-l",
+    "--list",
+    dest="is_list",
+    help="Checksum file is a JSON list of checksums",
+    action="store_true",
+)
+
 parser.add_argument("checksum_files", nargs=argparse.REMAINDER)
 
 args = parser.parse_args()
+
+if args.is_dict and args.is_list:
+    print("Checksum file cannot be list AND dict", file=sys.stderr)
+    parser.print_usage(file=sys.stderr)
+    sys.exit(1)
 
 if not len(args.checksum_files):
     print("At least one checksum file required", file=sys.stderr)
@@ -48,29 +71,44 @@ except AssistantConnectionError:
 ################################################################
 
 checksum_list = []
-checksum_mapping = {}
-paths = [path.rstrip(os.sep) for path in args.checksum_files]
-paths2 = []
-for path in paths:
-    if path.endswith(".CHECKSUM"):
-        checksum_file = path
-        path2 = os.path.splitext(path)[0]
+
+if args.is_dict or args.is_list:
+    checksums = json.load(open(args.checksum_files[0]))
+    for path in args.checksum_files[1:]:
+        curr_checksums = json.load(open(path))
+        if args.is_list:
+            checksums += curr_checksums
+        else:
+            checksums.update(curr_checksums)
+    if args.is_list:
+        checksum_list = checksums.copy()
     else:
-        checksum_file = path + ".CHECKSUM"
-        path2 = path
-    if os.path.exists(checksum_file) or path.endswith(".CHECKSUM"):
-        checksum = read_checksum_file(checksum_file)
-        if checksum is None:
-            err(f"File '{checksum_file}' does not contain a checksum")
-        checksum = Checksum(checksum).hex()
-    else:
-        try:
-            checksum = Checksum(path).hex()
-        except ValueError:
-            err(f"{path} is not an existing file, nor a valid checksum")
-    checksum_list.append(checksum)
-    paths2.append(path2)
-    checksum_mapping[path2] = checksum
+        checksum_list = list(checksums.values())
+else:
+    checksum_mapping = {}
+    paths = [path.rstrip(os.sep) for path in args.checksum_files]
+    paths2 = []
+
+    for path in paths:
+        if path.endswith(".CHECKSUM"):
+            checksum_file = path
+            path2 = os.path.splitext(path)[0]
+        else:
+            checksum_file = path + ".CHECKSUM"
+            path2 = path
+        if os.path.exists(checksum_file) or path.endswith(".CHECKSUM"):
+            checksum = read_checksum_file(checksum_file)
+            if checksum is None:
+                err(f"File '{checksum_file}' does not contain a checksum")
+            checksum = Checksum(checksum).hex()
+        else:
+            try:
+                checksum = Checksum(path).hex()
+            except ValueError:
+                err(f"{path} is not an existing file, nor a valid checksum")
+        checksum_list.append(checksum)
+        paths2.append(path2)
+        checksum_mapping[path2] = checksum
 
 buffer_sizes = {}
 for checksum in checksum_list:
@@ -86,7 +124,16 @@ for checksum in checksum_list:
             buffer_size = " " + buffer_size
     buffer_sizes[checksum] = buffer_size
 
-for path2 in paths2:
-    checksum = checksum_mapping[path2]
-    buffer_size = buffer_sizes[checksum]
-    print(f"{path2} {checksum}{buffer_size}")
+if args.is_dict:
+    for k, checksum in checksums.items():
+        buffer_size = buffer_sizes[checksum]
+        print(f"{k}{buffer_size}")
+elif args.is_list:
+    for checksum in checksums:
+        buffer_size = buffer_sizes[checksum]
+        print(f"{checksum}{buffer_size}")
+else:
+    for path2 in paths2:
+        checksum = checksum_mapping[path2]
+        buffer_size = buffer_sizes[checksum]
+        print(f"{path2} {checksum}{buffer_size}")
